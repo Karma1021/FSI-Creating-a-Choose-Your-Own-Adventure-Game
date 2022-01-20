9645c2fd70d5c3ac86f40bf9a9ecc851dc04e2ca
django daponte <djangodaponte@gmail.com>
2022-01-19 21:32:41 -0800

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your Code Here
+window.prompt('Do you head left or right?')
+let('left')  = window.prompt('left')
+{
+if (left > right)
+
+console.log=("you fell into a booby trap")
+
+
+else(left < right)
+{
+    console.log("you made it")
+}
